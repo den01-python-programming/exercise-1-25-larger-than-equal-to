751b7f4414739cc07a89ccb5e8ec0fad1d0b617b
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 09:15:42 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,34 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["5","25","35","10","55","55"]
+    input_values_stored = ["5","25","35","10","55","55"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert [output[0],output[1],output[2]] == ["Give the first number:","Give the second number:","Greater number is: " + input_values_stored[1]]
+    assert [output[3],output[4],output[5]] == ["Give the first number:","Give the second number:","Greater number is: " + input_values_stored[2]]
+    assert [output[6],output[7],output[8]] == ["Give the first number:","Give the second number:","The numbers are equal!"]
